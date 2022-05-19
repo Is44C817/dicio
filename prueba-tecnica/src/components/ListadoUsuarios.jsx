a27a5f5ff3a45ad9bf74242f94b44fd4c8838fbf
Is44C817 <isaac.sanchez.sandoval@gmail.com>
2022-05-19 02:28:13 -0500

    se agrega mensaje error en listadoUsuarios

--- prueba-tecnica/src/components/ListadoUsuarios.jsx
+++ prueba-tecnica/src/components/ListadoUsuarios.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col } from 'react-bootstrap'
+import {Container, Row, Col, Alert } from 'react-bootstrap'
 import Usuario from './Usuario';
 
 const ListadoUsuarios = ({usuarios}) => {
@@ -8,25 +8,39 @@ const ListadoUsuarios = ({usuarios}) => {
         <>
         <div className='md:w-1/2 lg:w-2/5 h-screen overflow-y-scroll'> 
             <Container>
-            <Row>
-                <Col>
-                    <h1 className='font-black text-2xl mx-auto'>Listado usuarios</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                {
-                    usuarios.map( usuario => (
-                        <Usuario 
-                        key={usuario.id}
-                        usuario={usuario}
-                        />
-                    ))
-                }
-                    
-   
-                </Col>
-            </Row>
+                {usuarios && usuarios.length ? (
+                    <>
+                        <Row>
+                            <Col>
+                                <h1 className='font-black text-2xl mx-auto'>Listado usuarios</h1>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                            {
+                                usuarios.map( usuario => (
+                                    <Usuario 
+                                    key={usuario.id}
+                                    usuario={usuario}
+                                    />
+                                ))
+                            }
+                            </Col>
+                        </Row>
+                    </>
+                ) : (
+                    <>
+                    <Row>
+                            <Col>
+                                <h1 className='font-black text-2xl mx-auto'>Listado usuarios</h1>
+                            </Col>
+                    </Row>
+                    <Alert  variant='danger'>
+                        No hay usuarios
+                    </Alert>
+                    </>
+                )}
+
             </Container>
         </div>
         </>
